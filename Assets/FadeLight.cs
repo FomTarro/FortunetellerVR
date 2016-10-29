3bcd6fc05313c5b435226607675536fd7bc07afa
bonbombs <kelly.zhang2306@gmail.com>
2016-10-29 14:10:33 -0400

    Lots of stuff like states and UI and more

---
+++ Assets/FadeLight.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FadeLight : MonoBehaviour {
+
+    Light light;
+
+    [SerializeField]
+    float maxIntensity;
+    bool fadeIn = true;
+
+	// Use this for initialization
+	void Start () {
+        light = GetComponent<Light>();
+        light.intensity = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (fadeIn)
+        {
+            if (light.intensity > maxIntensity)
+                fadeIn = false;
+            light.intensity += 0.005f;
+        } else
+        {
+            if (light.intensity <= 0)
+                fadeIn = true;
+            light.intensity -= 0.005f;
+        }
+	}
+}
